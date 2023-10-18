65bed8051d07f07eca6f962f0d281a551a3de6ec
Frederico Ferrarezi <eng.producaoffdc@gmail.com>
2023-10-18 19:55:30 -0300

    18/10

---
+++ classes.py
@@ -0,0 +1,28 @@
+"""
+POO - Classes
+
+Em poo, classes são modelos dos objetos do mundo real.
+
+imagine que você queira fazer um sistema para automatizar o controle
+das lâmpadas da sua casa.
+
+"""
+class lampada:
+    pass
+
+
+class ContaCorrente:
+    pass
+
+class Produto:
+    pass
+
+class Usuario:
+    pass
+
+
+lamp = lampada
+
+print(lamp)
+print(type(lamp))
+ 
---
+++ poo.py
@@ -0,0 +1,33 @@
+"""
+Programação orientada a objetos - POO
+
+- POO é uma paradigma de programção que utiliza que mapeamento de objetos do mundo real
+para modelos computacionais.
+
+- Paradigma de programlção é a forma metodologia utilizada para pensar/desenvolver sistemas
+
+- Principais elementos da orientação do objeto
+
+- Classe
+- Atributos
+- Metodos
+- Construtor
+- Objeto 
+"""
+#Exemplo
+
+numero = 10
+
+print(numero)
+print(type(numero))
+
+nome = 'Geek'
+print(nome)
+print(type(nome))
+
+class Produto:
+    pass
+
+ps4 = Produto()
+print(ps4)
+print(type(ps4))
